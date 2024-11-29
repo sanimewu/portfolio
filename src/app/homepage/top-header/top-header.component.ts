80953687412544dde30b34d5b0cdf99713b4376a
sanimewu <2020-1-60-267@std.ewubd.edu>
2024-11-29 11:55:28 +0600

    #1

--- src/app/homepage/top-header/top-header.component.ts
+++ src/app/homepage/top-header/top-header.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-top-header',
   templateUrl: './top-header.component.html',
   styleUrl: './top-header.component.scss'
 })
-export class TopHeaderComponent {
-  logoName:string = "Md. Sanim Hossain";
+export class TopHeaderComponent implements OnInit {
+  logoName:string = "Md.Sanim Hossain";
   isNavbarOpen: boolean = false;
+  screenIsMdOrLarger: boolean = false;
 
   toggleNavbar(): void {
     this.isNavbarOpen = !this.isNavbarOpen;
   }
+  ngOnInit(): void {
+    this.checkScreenSize();
+  }
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkScreenSize();
+  }
+  checkScreenSize(): void {
+    this.screenIsMdOrLarger = window.innerWidth >= 768; // `md` breakpoint in Tailwind (768px)
+  }
+
 }
