dd55cec159b260c6f5f7d3498874ad964e715364
sanimewu <2020-1-60-267@std.ewubd.edu>
2024-12-17 04:03:24 +0600

    #blog

---
+++ src/app/homepage/blog/blog.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {NgForOf, NgOptimizedImage} from "@angular/common";
+import {ScrollAnimateDirective} from "../scroll-animate.directive";
+
+@Component({
+  selector: 'app-blog',
+  standalone: true,
+    imports: [
+        NgForOf,
+        NgOptimizedImage,
+        ScrollAnimateDirective
+    ],
+  templateUrl: './blog.component.html',
+  styleUrl: './blog.component.scss'
+})
+export class BlogComponent {
+
+}
