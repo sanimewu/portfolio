3268037aa2d7d1a1af65538f13fa69a9f995f750
sanimewu <2020-1-60-267@std.ewubd.edu>
2024-12-17 13:22:50 +0600

    version 1.0

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TCIHFY6T.mjs";import"./chunk-ZMZA5E57.mjs";import"./chunk-VSQJZ5DW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-UA2D25CX.mjs";import"./chunk-ZMZA5E57.mjs";import"./chunk-VSQJZ5DW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
